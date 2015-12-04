e35ea4aa1217cca92bf07d8c95924e18899cd132
Chris <chrl@spreadshirt.net>
2015-12-04 11:16:29 +0100

    Created a BowerSearchProvider sub class.

---
+++ src/org/chrisle/netbeans/plugins/nbquicksearchextender/BowerSearchProvider.java
@@ -0,0 +1,18 @@
+package org.chrisle.netbeans.plugins.nbquicksearchextender;
+
+import org.netbeans.spi.quicksearch.SearchRequest;
+import org.netbeans.spi.quicksearch.SearchResponse;
+
+
+public class BowerSearchProvider extends QuickSearchProvider {
+    public BowerSearchProvider() {
+        super.setBaseUrl("");
+        super.setSearchUrl("http://bower.io/search/?q=");
+        super.setSelector(".search-results td a");
+    }
+
+    @Override
+    public void evaluate(SearchRequest request, SearchResponse response) {
+        super.evaluate(request, response); //To change body of generated methods, choose Tools | Templates.
+    }
+}
