61bbce57a0e691154afb0bab97a8a0675026833a
Chris <chrl@spreadshirt.net>
2015-12-04 11:16:12 +0100

    Created a JarSearchProvider sub class.

---
+++ src/org/chrisle/netbeans/plugins/nbquicksearchextender/JarSearchProvider.java
@@ -0,0 +1,17 @@
+package org.chrisle.netbeans.plugins.nbquicksearchextender;
+
+import org.netbeans.spi.quicksearch.SearchRequest;
+import org.netbeans.spi.quicksearch.SearchResponse;
+
+
+public class JarSearchProvider extends QuickSearchProvider {
+    public JarSearchProvider() {
+        super.setSearchUrl("http://www.findjar.com/index.x?query=");
+        super.setSelector(".results a");
+    }
+
+    @Override
+    public void evaluate(SearchRequest request, SearchResponse response) {
+        super.evaluate(request, response); //To change body of generated methods, choose Tools | Templates.
+    }
+}
