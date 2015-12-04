87aa6bfee7d9b489ce6f4620fc8935b1fb5c6abb
Chris <chrl@spreadshirt.net>
2015-12-04 11:15:41 +0100

    Created a NpmSearchProvider sub class.

---
+++ src/org/chrisle/netbeans/plugins/nbquicksearchextender/NpmSearchProvider.java
@@ -0,0 +1,18 @@
+package org.chrisle.netbeans.plugins.nbquicksearchextender;
+
+import org.netbeans.spi.quicksearch.SearchRequest;
+import org.netbeans.spi.quicksearch.SearchResponse;
+
+
+public class NpmSearchProvider extends QuickSearchProvider {
+    public NpmSearchProvider() {
+        super.setBaseUrl("https://www.npmjs.com/");
+        super.setSearchUrl("https://www.npmjs.com/search?q=");
+        super.setSelector(".search-results .package-details a");
+    }
+
+    @Override
+    public void evaluate(SearchRequest request, SearchResponse response) {
+        super.evaluate(request, response);
+    }
+}
