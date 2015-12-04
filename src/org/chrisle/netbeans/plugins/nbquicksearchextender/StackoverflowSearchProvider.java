4ab7dab934f93a11250eb0df780ad1b75f37a8f2
Chris <chrl@spreadshirt.net>
2015-12-04 11:15:51 +0100

    Created a StackoverflowSearchProvider sub class.

---
+++ src/org/chrisle/netbeans/plugins/nbquicksearchextender/StackoverflowSearchProvider.java
@@ -0,0 +1,18 @@
+package org.chrisle.netbeans.plugins.nbquicksearchextender;
+
+import org.netbeans.spi.quicksearch.SearchRequest;
+import org.netbeans.spi.quicksearch.SearchResponse;
+
+
+public class StackoverflowSearchProvider extends QuickSearchProvider {
+
+    public StackoverflowSearchProvider() {
+        super.setSearchUrl("http://stackoverflow.com/search?q=");
+        super.setSelector(".summary>.result-link a");
+    }
+
+    @Override
+    public void evaluate(SearchRequest request, SearchResponse response) {
+        super.evaluate(request, response); //To change body of generated methods, choose Tools | Templates.
+    }
+}
